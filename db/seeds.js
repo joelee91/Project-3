ca2f90d4a0680320bef15ba8d88effda391e9955
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-11-28 03:58:33 -0500

    updated seeds

--- db/seeds.js
+++ db/seeds.js
@@ -6,14 +6,18 @@ const clean = new ToDo({
     task: 'clean out fridge'
 })
 
+const fix = new ToDo({
+    task: 'fix the tv'
+})
+
 const joe = new User({
     name: 'Joe Lee',
-    toDo: clean
+    toDo: [clean, fix]
 })
 
 User.remove({})
     .then(() => ToDo.remove({}))
-    .then(() => ToDo.insertMany(clean))
+    .then(() => ToDo.insertMany([clean, fix]))
     .then(() => joe.save())
     .then(() => console.log('Successful Save'))
     .then(() => mongoose.connection.close())
