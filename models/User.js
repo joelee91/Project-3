30b372f23bd53249e712ea16d424b87f1d0b86d8
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-11-28 02:43:41 -0500

    created user model

--- models/User.js
+++ models/User.js
@@ -3,5 +3,11 @@ const Schema = mongoose.Schema
 
 const User = new Schema({
     name: String,
-    
+    image: String,
+    toDo: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'ToDo'
+        }
+    ]
 })
